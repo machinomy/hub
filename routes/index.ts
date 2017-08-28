87db7d709460ad3f8dafeff948fd7f3f7b8bed4c
burtovoy <volgobur@gmail.com>
2017-08-28 18:43:17 +0300

    Add ecosystem

--- routes/index.ts
+++ routes/index.ts
@@ -6,7 +6,7 @@ import Web3 = require('web3')
 import machinomy from 'machinomy/index'
 let settings = machinomy.configuration.receiver()
 let web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
-// web3.personal.unlockAccount(settings.account, settings.password, 1000)
+web3.personal.unlockAccount(settings.account, settings.password, 1000)
 const hub = new Hub(web3, settings.account, null)
 
 router.post('/machinomy', hub.payment())
