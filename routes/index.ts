b6bed8459e4febe47aada819eac1a8cd9ca81321
burtovoy <volgobur@gmail.com>
2017-09-01 16:54:32 +0300

    Git add mongo integration

--- routes/index.ts
+++ routes/index.ts
@@ -2,16 +2,25 @@ import * as express from 'express';
 export let router = express.Router();
 
 import {Hub} from 'machinomy/lib/hub'
+import Storage from 'machinomy/lib/storage'
 import Web3 = require('web3')
 import machinomy from 'machinomy/index'
-let settings = machinomy.configuration.receiver()
+import mongo from 'machinomy/lib/mongo'
 let web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'))
+let settings = machinomy.configuration.receiver()
 web3.personal.unlockAccount(settings.account, settings.password, 1000)
-const hub = new Hub(web3, settings.account, null)
+let storage = new Storage(web3, settings.databaseFile, "receiver", true, settings.engine);
+const hub = new Hub(web3, settings.account, storage);
 
-router.post('/machinomy', hub.payment())
-
-router.get('/verify', hub.verify())
+if (settings.engine == "mongo") {
+  mongo.connectToServer(() => {
+    router.post('/machinomy', hub.payment())
+    router.get('/verify', hub.verify())
+  });
+} else {
+  router.post("/machinomy", hub.payment());
+  router.get("/verify", hub.verify());
+}
 
 router.get('/isalive', (req: express.Request, res: express.Response, next: express.NextFunction):any => {
   res.send('yes')
