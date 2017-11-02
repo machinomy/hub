b58c435a78ab5ea7b8c1735e073e2d1ce8e52199
Alexandr Burtovoy <volgobur@gmail.com>
2017-11-02 20:32:00 +0300

    fix 404

--- app.ts
+++ app.ts
@@ -29,7 +29,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('v1/', routes);
+app.use('/v1', routes);
 
 // catch 404 and forward to error handler
 app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
