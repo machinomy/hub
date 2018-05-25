93b3d1d28a6ceaa742719a12b096ecea80b78fbb
Sergey Ukustov <sergey.ukustov@machinomy.com>
2018-05-25 14:05:01 +0300

    Or not use

--- app.ts
+++ app.ts
@@ -38,7 +38,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/.well-known/machinomy', routes)
+app.use('/v1', routes)
 
 // catch 404 and forward to error handler
 app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
