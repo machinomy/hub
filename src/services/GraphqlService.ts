70930587d9674ea3492dcae2236b1d1d8630c38f
Sergey Ukustov <sergey.ukustov@machinomy.com>
2018-06-20 13:28:25 +0300

    Dummy Graphql Service

--- src/services/GraphqlService.ts
+++ src/services/GraphqlService.ts
@@ -2,14 +2,39 @@ import { GraphQLSchema } from 'graphql'
 import { makeExecutableSchema } from 'graphql-tools'
 
 const typeDefs = `
-  type Query { books: [Book] }
-  type Book { title: String, author: String }
+  type Query {
+    books: [Book]
+  }
+  
+  type Mutation {
+    createNonce(address: String!): Nonce
+  }
+  
+  type Book {
+    title: String
+    author: String
+  }
+  
+  type Nonce {
+    value: String!
+    address: String!
+    until: String!
+  }
 `
 
 const resolvers = {
   Query: {
     books: () => [{title: 'The Daemons', author: 'Dostoyevsky'}]
   },
+  Mutation: {
+    createNonce: (address: string) => {
+      return {
+        value: 'nonce',
+        address: '0xdead',
+        until: '1H'
+      }
+    }
+  }
 }
 
 export default class GraphqlService {
