d7318c3b3ccd37e004fe474007265c3b6dfee7c6
Andrei Riaskov <ryaskov.work@gmail.com>
2018-07-12 20:07:44 +0300

    Fix database availability check.

--- src/bin/hub.ts
+++ src/bin/hub.ts
@@ -4,10 +4,6 @@ import { Hub, Configuration } from '../index'
 
 async function main () {
   let configuration = await Configuration.env()
-  const databaseAvailable = await Configuration.checkDatabaseAvailability(configuration.databaseUrl)
-  if (databaseAvailable !== true) {
-    throw Error(`Database is not available via URL "${configuration.databaseUrl}"!`)
-  }
   let hub = await Hub.build(configuration)
   return hub.start()
 }
